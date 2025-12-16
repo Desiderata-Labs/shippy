eafbcd527c9bde1145c6758cf19c43053ff5779c
rob phillips <rob@robphillips.me>
2025-12-15 19:32:19 -0500

    [analytics] plausible

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,21 +1,24 @@
 'use client'
 
 import { TRPCProvider } from '@/lib/trpc/react'
+import PlausibleProvider from 'next-plausible'
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from 'sonner'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      forcedTheme="dark"
-      disableTransitionOnChange
-    >
-      <TRPCProvider>
-        {children}
-        <Toaster position="bottom-right" richColors />
-      </TRPCProvider>
-    </ThemeProvider>
+    <PlausibleProvider domain="shippy.sh">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        forcedTheme="dark"
+        disableTransitionOnChange
+      >
+        <TRPCProvider>
+          {children}
+          <Toaster position="bottom-right" richColors />
+        </TRPCProvider>
+      </ThemeProvider>
+    </PlausibleProvider>
   )
 }
